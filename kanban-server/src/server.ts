43d0f2b637346e89d86d9e98df2b095b517c9b2a
Olha Murava <olia.mur2006@gmail.com>
2025-05-21 17:10:31 +0300

    first commit

---
+++ kanban-server/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import boardRoutes from './routes/board.routes';
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use('/api', boardRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
